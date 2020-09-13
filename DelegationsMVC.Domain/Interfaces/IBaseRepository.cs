7397f234a172bf18731f99262bfd0c4c04ceba5b
mrzepecki <m.rzepaa@gmail.com>
2020-09-13 16:27:00 +0200

    Add BaseRepostiory

---
+++ DelegationsMVC.Domain/Interfaces/IBaseRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DelegationsMVC.Domain.Interfaces
+{
+    public interface IBaseRepository<T> where T: class
+    {
+        void AddEntity(T entityToAdd);
+        void RemoveEntity(int entityToRemoveId);
+        T GetEntityById(int entityId);
+        void AddRange(IEnumerable<T> entitiesToAdd);
+        void RemoveRange(IEnumerable<T> itemsToRemove);
+        T GetEntityByExpression(Expression<Func<T, bool>> expression);
+        IQueryable<T> GetAllEntitites();
+    }
+}
---
+++ DelegationsMVC.Infrastructure/Repositories/BaseRepository.cs
@@ -0,0 +1,60 @@
+﻿using DelegationsMVC.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DelegationsMVC.Infrastructure.Repositories
+{
+    public class BaseRepository<T> : IBaseRepository<T> where T : class
+    {
+        private readonly Context _context;
+        public BaseRepository(Context context)
+        {
+            _context = context;
+        }
+        public void AddEntity(T entityToAdd)
+        {
+            _context.Set<T>().Add(entityToAdd);
+            _context.SaveChanges();
+        }
+
+        public void AddRange(IEnumerable<T> entitiesToAdd)
+        {
+            _context.Set<T>().AddRange(entitiesToAdd);
+            _context.SaveChanges();
+        }
+
+        public IQueryable<T> GetAllEntitites()
+        {
+            return _context.Set<T>();
+        }
+
+        public T GetEntityByExpression(Expression<Func<T, bool>> expression)
+        {
+            return _context.Set<T>().FirstOrDefault(expression);
+        }
+
+        public T GetEntityById(int entityId)
+        {
+            return _context.Set<T>().Find(entityId);
+        }
+
+        public void RemoveEntity(int entityToRemoveId)
+        {
+            var entityToRemove = GetEntityById(entityToRemoveId);
+            if(entityToRemove != null)
+            {
+                _context.Set<T>().Remove(entityToRemove);
+                _context.SaveChanges();
+            }
+        }
+
+        public void RemoveRange(IEnumerable<T> entitiesToRemove)
+        {
+            _context.Set<T>().RemoveRange(entitiesToRemove);
+            _context.SaveChanges();
+        }
+    }
+}
