10c0601a8dda82265798ee70cb09eb753ae53abf
mrzepecki <m.rzepaa@gmail.com>
2020-09-16 20:33:21 +0200

    Add employee repository

--- DelegationsMVC.Infrastructure/Repositories/EmployeeRepository.cs
+++ DelegationsMVC.Infrastructure/Repositories/EmployeeRepository.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using DelegationsMVC.Domain.Model;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace DelegationsMVC.Infrastructure.Repositories
@@ -11,6 +13,41 @@ namespace DelegationsMVC.Infrastructure.Repositories
         public EmployeeRepository(Context context)
         {
             _context = context;
+        }
+
+        public int AddEmployee(Employee employee)
+        {
+            _context.Employees.Add(employee);
+            _context.SaveChanges();
+            return employee.Id;
+        }
+
+        public void DeleteEmployee(int employeeId)
+        {
+            var employee = _context.Employees.Find(employeeId);
+            if(employee != null)
+            {
+                _context.Employees.Remove(employee);
+                _context.SaveChanges();
+            }
+        }
+
+        public Employee GetEmployeeById(int id)
+        {
+            var employee = _context.Employees.FirstOrDefault(e => e.Id == id);
+            return employee;
+        }
+
+        public IQueryable<Employee> GetEmployeesByType(int typeId)
+        {
+            var employees = _context.EmployeeTypes.FirstOrDefault(et => et.Id == typeId).Employees.AsQueryable();
+            return employees;
+        }
+
+        public IQueryable<ContactDetail> GetEmployeeContactDetails(int employeeId)
+        {
+            var contactDetails = _context.Employees.FirstOrDefault(e => e.Id == employeeId).ContactDetails.AsQueryable();
+            return contactDetails;
         }
     }
 }
