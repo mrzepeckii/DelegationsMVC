f57f9c6d70a343309c37442e369ede70153604b7
mrzepecki <m.rzepaa@gmail.com>
2020-09-16 20:25:09 +0200

    Add destination repository

---
+++ DelegationsMVC.Infrastructure/Repositories/EmployeeRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DelegationsMVC.Infrastructure.Repositories
+{
+    public class EmployeeRepository
+    {
+        private readonly Context _context;
+
+        public EmployeeRepository(Context context)
+        {
+            _context = context;
+        }
+    }
+}
