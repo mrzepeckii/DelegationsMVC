aa95955b779efcc42364d9aae49ad1a42599a659
mrzepecki <m.rzepaa@gmail.com>
2020-11-09 20:09:42 +0100

    Update views -> better presentation of view

--- DelegationsMVC.Application/ViewModels/DelegationVm/DelegationForListVm.cs
+++ DelegationsMVC.Application/ViewModels/DelegationVm/DelegationForListVm.cs
@@ -18,7 +18,8 @@ namespace DelegationsMVC.Application.ViewModels.DelegationVm
         public void Mapping(Profile profile)
         {
             profile.CreateMap<Delegation, DelegationForListVm>()
-                .ForMember(s => s.DelegationNo, opt => opt.MapFrom(d => d.Employee.FirstName + d.Employee.LastName + d.CreatedDateTime))
+                .ForMember(s => s.DelegationNo, opt => opt.MapFrom(d => d.Employee.FirstName.Substring(0,1).ToUpper() + d.Employee.LastName.Substring(0,1).ToUpper()
+                    + d.Routes.OrderBy(r => r.RouteDetail.StartDate).First().RouteDetail.StartDate.Date.ToString("ddMMyyyy")))
                 .ForMember(s => s.StartDate, opt => opt.MapFrom(d => d.Routes.OrderBy(r => r.RouteDetail.StartDate).First().RouteDetail.StartDate))
                 .ForMember(s => s.EndDate, opt => opt.MapFrom(d => d.Routes.OrderByDescending(r => r.RouteDetail.EndDate).First().RouteDetail.EndDate))
                 .ForMember(s => s.Destination, opt => opt.MapFrom(d => d.Destination.Name))
