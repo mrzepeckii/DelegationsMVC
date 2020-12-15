ce48d733c8ca3d9dcad7dea252c4f54be1dfde99
mrzepecki <m.rzepaa@gmail.com>
2020-12-15 20:10:22 +0100

    Add employee api controller funcionality

---
+++ DelegationApi/Controllers/EmployeeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DelegationsMVC.Application.Interfaces;
+using DelegationsMVC.Application.ViewModels.EmployeeVm;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DelegationApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IEmployeeService _empService;
+
+        public EmployeeController(IEmployeeService empService)
+        {
+            _empService = empService;
+        }
+
+        [HttpGet]
+        public ActionResult<ListEmployeeForListVm> Get()
+        {
+            var result = _empService.GetAllEmployeeForList();
+            if(result == null)
+            {
+                return NotFound();
+            }
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<EmployeeDetailVm> Get(int id)
+        {
+            var result = _empService.GetEmployeeDetails(id);
+            if(result == null)
+            {
+                return NotFound();
+            }
+            return Ok(result);
+        }
+    }
+}
