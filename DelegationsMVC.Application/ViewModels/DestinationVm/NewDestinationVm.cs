a96eff478237616debfdd78a1477b0d651ceb610
mrzepecki <m.rzepaa@gmail.com>
2021-01-02 13:00:23 +0100

    Add validation of new destination and new project

--- DelegationsMVC.Application/ViewModels/DestinationVm/NewDestinationVm.cs
+++ DelegationsMVC.Application/ViewModels/DestinationVm/NewDestinationVm.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using DelegationsMVC.Application.Mapping;
 using DelegationsMVC.Domain.Model;
+using FluentValidation;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -17,6 +18,17 @@ namespace DelegationsMVC.Application.ViewModels.DestinationVm
         public void Mapping(Profile profile)
         {
             profile.CreateMap<Destination, NewDestinationVm>().ReverseMap();
+        }
+    }
+
+    public class NewDestinationValidation : AbstractValidator<NewDestinationVm>
+    {
+        public NewDestinationValidation()
+        {
+            RuleFor(d => d.Id).NotNull();
+            RuleFor(d => d.Name).NotEmpty().WithMessage("Nazwa nie może pozostać pusta")
+                .MaximumLength(255).WithMessage("Maksymalna dlugosc to 255 znaków");
+            RuleFor(d => d.CountryId).NotNull().WithMessage("Wybierz kraj");
         }
     }
 }
--- DelegationsMVC.Application/ViewModels/DestinationVm/NewProjectVm.cs
+++ DelegationsMVC.Application/ViewModels/DestinationVm/NewProjectVm.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using DelegationsMVC.Application.Mapping;
 using DelegationsMVC.Domain.Model;
+using FluentValidation;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -20,6 +21,18 @@ namespace DelegationsMVC.Application.ViewModels.DestinationVm
         public void Mapping(Profile profile)
         {
             profile.CreateMap<Project, NewProjectVm>().ReverseMap();
+        }
+    }
+
+    public class NewProjectValidation : AbstractValidator<NewProjectVm>
+    {
+        public NewProjectValidation()
+        {
+            RuleFor(p => p.Id).NotNull();
+            RuleFor(p => p.Name).NotEmpty().WithMessage("Nazwa nie może pozostać pusta")
+                .MaximumLength(255).WithMessage("Maksymalna długość wynosi 255 znaków");
+            RuleFor(p => p.Number).NotEmpty().WithMessage("Numer nie może pozostac pusty");
+            RuleFor(p => p.DestinationId).NotNull().WithMessage("Wybierz klienta");
         }
     }
 }
